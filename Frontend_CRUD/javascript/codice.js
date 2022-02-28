8cc1dbfa93df47cc34c99f4a0a02a749f29a6b67
lai2-01 <lai2-01@itismeucci.com>
2022-02-28 11:48:46 +0100

    c

--- Frontend_CRUD/javascript/codice.js
+++ Frontend_CRUD/javascript/codice.js
@@ -1,51 +1,6 @@
-//file json
-var data = [
-    {
-      "id": 10001,
-      "birthDate": "1953-09-01",
-      "firstName": "Georgi",
-      "lastName": "Facello",
-      "gender": "M",
-      "hireDate": "1986-06-25",
-    },
-    {
-      "id": 10002,
-      "birthDate": "1964-06-01",
-      "firstName": "Bezalel",
-      "lastName": "Simmel",
-      "gender": "F",
-      "hireDate": "1985-11-20",
-    },
-    {
-      "id": 10003,
-      "birthDate": "1959-12-02",
-      "firstName": "Parto",
-      "lastName": "Bamford",
-      "gender": "M",
-      "hireDate": "1986-08-27T22:00:00.000+0000",
-    },
-    {
-      "id": 10004,
-      "birthDate": "1954-04-30",
-      "firstName": "Chirstian",
-      "lastName": "Koblick",
-      "gender": "M",
-      "hireDate": "1986-11-30",
-
-    },
-    {
-      "id": 10005,
-      "birthDate": "1955-01-20",
-      "firstName": "Kyoichi",
-      "lastName": "Maliniak",
-      "gender": "M",
-      "hireDate": "1989-09-11T22:00:00.000+0000",
-
-    }
-  ];
-
+//non piu' file json
+var data = [];
 var nextID = 10006;
-
 
 //crea tabella
 function tabellaCodice(){
@@ -56,21 +11,27 @@ function tabellaCodice(){
     righe = righe + "<td>" + value.firstName + "</td>";
     righe = righe + "<td>" + value.lastName + "</td>";
     righe = righe + "<td>" + value.gender + "</td>";
+    righe = righe + "<td>" + "<input type='button' class='btn btn-warning' value='MODIFICA' onclick='modifica("+value.id+")' id='"+value.id+"'>";
     righe = righe + "<td>" + "<input type='button' class='btn btn-danger' value='RIMUOVI' onclick='elimina("+value.id+")' id='"+value.id+"'>";
     righe = righe + "</tr>";
  });
     $("#tbody").append(righe);
-
 }
 
+//tabella presa tramite il server
 $(document).ready(function (){
-    tabellaCodice();
+    $.get("http://localhost:8080/employees", function(contenuto){
+      data = contenuto["_embedded"]["employees"];
+      tabellaCodice();
+    })
 });
 
+//elimina 
 function elimina(id){
     $("#"+id).closest("tr").remove();
 }
 
+//aggiungi
 $(document).ready(function (){
     $('#btn').click(function(){
         var righeadd = "";
@@ -83,20 +44,26 @@ $(document).ready(function (){
             "firstName": firstName,
             "gender": gender,
             "hireDate": "",
-
         })
-        
-
     righeadd +="<tr>";
     righeadd +=  "<th>" + nextID + "</th>";
     righeadd +="<td>" + firstName + "</td>";
     righeadd +="<td>" + lastName + "</td>";
     righeadd +="<td>" + gender + "</td>";
+    righeadd +="<td>" + "<input type='button' class='btn btn-warning' value='MODIFICA' onclick='modifica("+nextID+")' id='"+nextID+"'>";
     righeadd +="<td>" + "<input type='button' class='btn btn-danger' value='RIMUOVI' onclick='elimina("+nextID+")' id='"+nextID+"'>";
     righeadd +="</tr>";
     nextID++;
     $('#tbody').append(righeadd);
     });
 });
+
+//modifica
+$(document).ready(function () {
+  $('#dtBasicExample').DataTable();
+  $('.dataTables_length').addClass('bs-select');
+});
+
+
 
 
